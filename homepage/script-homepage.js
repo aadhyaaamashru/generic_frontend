ecc660e232dfae2dff402565c9f366b121c60245
Aadhyaaa Mashru <aadhyaaamashru@yahoo.com>
2023-08-11 20:41:03 -0700

    contact-us basic

--- homepage/script-homepage.js
+++ homepage/script-homepage.js
@@ -1,15 +1,31 @@
-let counterValueDisplays = document.querySelectorAll(".counter-num");
-let interval = 4000;
-
-counterValueDisplays.forEach((valueDisplay) => {
+// Function to start the counter
+function startCounter(counterElement, endValue, duration) {
   let startValue = 0;
-  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-  let duration = Math.floor(interval / endValue);
+  let interval = Math.floor(duration / endValue);
   let counter = setInterval(function () {
     startValue += 1;
-    valueDisplay.textContent = startValue;
-    if (startValue >= endValue) { // Use greater than or equal to
+    counterElement.textContent = startValue;
+    if (startValue >= endValue) {
       clearInterval(counter);
     }
-  }, duration);
+  }, interval);
+}
+
+// Use Intersection Observer to trigger counter when elements are in the viewport
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      let counterNumElement = entry.target.querySelector(".counter-num");
+      let endValue = parseInt(counterNumElement.getAttribute("data-val"));
+      let duration = 4000; // Set your desired duration here
+      startCounter(counterNumElement, endValue, duration);
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+// Observe each counter container
+let counterContainers = document.querySelectorAll(".counter-container");
+counterContainers.forEach((container) => {
+  observer.observe(container);
 });
